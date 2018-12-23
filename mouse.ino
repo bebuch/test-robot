e39c0df660ad38a6691d93c199dca4edc3228240
Benjamin Buch <benni.buch@gmail.com>
2018-12-23 18:22:00 +0100

    program move via buttons

--- mouse.ino
+++ mouse.ino
@@ -1,4 +1,8 @@
 #include <IRremote.h>
+#include <CircularBuffer.h>
+
+#undef min
+#undef max
 
 #include "lib/arduino-tools/include/stepper_state_8.hpp"
 
@@ -14,7 +18,12 @@ struct interrupt_guard{
 };
 
 
-enum class action{
+constexpr unsigned min(unsigned l, unsigned r){
+  return l < r ? l : r;
+}
+
+
+enum class action: unsigned char{
   none,
   forward,
   backward,
@@ -28,10 +37,12 @@ public:
     interrupt_guard lock;
     action_ = act;
     steps_ = steps;
+    done_steps_ = 0;
   }
 
   void step_action(){
     auto steps = steps_;
+    auto done_steps = done_steps_;
     if(steps == 0){
       left_.unset();
       right_.unset();
@@ -61,20 +72,25 @@ public:
     right_.set();
 
     --steps;
+    ++done_steps;
     if(steps == 0){
       action_ = action::none;
     }
 
     steps_ = steps;
+    done_steps_ = done_steps;
   }
 
   unsigned step_delay_us(){
-    return max_step_delay_us;
+    constexpr unsigned max_diff = max_step_delay_us - min_step_delay_us;
+    return max_step_delay_us - min(done_steps_, min(steps_, max_diff));
   }
 
 private:
-  static constexpr unsigned max_step_delay_us = 1000;
+  static constexpr unsigned max_step_delay_us = 1200;
+  static constexpr unsigned min_step_delay_us = 650;
 
+  unsigned long volatile done_steps_ = 0;
   unsigned long volatile steps_ = 0;
   action volatile action_ = action::none;
   tools::stepper_state_8<  5,  6,  7,  8 > right_;
@@ -138,51 +154,63 @@ void loop(){
   unsigned long last_ir_button = 0;
   unsigned long last_ir_delay = millis();
 
+  CircularBuffer< action, 64 > movements;
+  bool program_mode = true;
+
   for(;;){
-    if(digitalRead(15) == HIGH){
-      chassis.move(action::turn_right, 3200);
-      delay(250);
-    }else if(digitalRead(13) == HIGH){
-      chassis.move(action::backward, 3200);
-      delay(250);
-    }else if(digitalRead(14) == HIGH){
-      chassis.move(action::forward, 3200);
-      delay(250);
-    }else if(digitalRead(17) == HIGH){
-      chassis.move(action::turn_left, 3200);
-      delay(250);
-    }else if(digitalRead(16) == HIGH){
-      chassis.move(action::none, 0);
-      delay(250);
-    }
+    if(program_mode){
+      if(digitalRead(15) == HIGH){
+        movements.push(action::turn_right);
+        delay(250);
+      }else if(digitalRead(13) == HIGH){
+        movements.push(action::backward);
+        delay(250);
+      }else if(digitalRead(14) == HIGH){
+        movements.push(action::forward);
+        delay(250);
+      }else if(digitalRead(17) == HIGH){
+        movements.push(action::turn_left);
+        delay(250);
+      }else if(digitalRead(16) == HIGH){
+        program_mode = false;
+        delay(250);
+      }else{
+        decode_results results;
+        if(irrecv.decode(&results)){
+          unsigned long const now = millis();
+          if(last_ir_button != results.value || last_ir_delay < now){
+            last_ir_delay = now + min_ir_pause_ms;
+            last_ir_button = results.value;
 
-    decode_results results;
-    if(irrecv.decode(&results)){
-      unsigned long const now = millis();
-      if(last_ir_button != results.value || last_ir_delay < now){
-        last_ir_delay = now + min_ir_pause_ms;
-        last_ir_button = results.value;
+            switch(last_ir_button){
+              case 0xE0E006F9ul:
+                chassis.move(action::forward, 3200);
+              break;
+              case 0xE0E046B9ul:
+                chassis.move(action::turn_right, 3200);
+              break;
+              case 0xE0E08679ul:
+                chassis.move(action::backward, 3200);
+              break;
+              case 0xE0E0A659ul:
+                chassis.move(action::turn_left, 3200);
+              break;
+              case 0xE0E016E9ul:
+                chassis.move(action::none, 0);
+              break;
+            }
+          }
 
-        switch(last_ir_button){
-          case 0xE0E006F9ul:
-            chassis.move(action::forward, 3200);
-          break;
-          case 0xE0E046B9ul:
-            chassis.move(action::turn_right, 3200);
-          break;
-          case 0xE0E08679ul:
-            chassis.move(action::backward, 3200);
-          break;
-          case 0xE0E0A659ul:
-            chassis.move(action::turn_left, 3200);
-          break;
-          case 0xE0E016E9ul:
-            chassis.move(action::none, 0);
-          break;
+          irrecv.resume();
         }
       }
+    }else{
+      while(movements.size()){
+        chassis.move(movements.pop(), 3200);
+        delay(3500);
+      }
 
-      irrecv.resume();
+      program_mode = true;
     }
   }
 }
